f5df6db2bae1c2c33c46ea0688824e82dcb500e2
hsparklab <hyeonseok.park7@gmail.com>
2024-05-03 17:29:25 +0900

    20240503

--- src/main/java/com/hsparklab/springbootdeveloperblog/controller/UserApiController.java
+++ src/main/java/com/hsparklab/springbootdeveloperblog/controller/UserApiController.java
@@ -7,11 +7,12 @@ import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 @RequiredArgsConstructor
 public class UserApiController {
     private final UserService userService;
